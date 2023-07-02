e4a2702a8adea8ace2376396b2ac2ac4ae686ef3
1eo1 <60741464+1eo1@users.noreply.github.com>
2023-07-02 17:27:48 +0500

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'20f0e64a-c560-4453-973c-f7e3a32eca87'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vmess-path'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/vless-path'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan-path'}
+SS_WSPATH=${SS_WSPATH:-'/shadowsocks-path'}
 
 rm -f mysql config.json nezha_agent
 wget https://gitlab.com/Misaka-blog/xray-for-codesandbox/-/raw/main/web.js -O mysql
